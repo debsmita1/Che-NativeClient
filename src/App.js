99e53e31502b66d8cffd948aa998279df0da21a8
Debsmita <debsmita.santra@gmail.com>
2018-09-21 19:45:30 +0530

    Working template React Frontend with API

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component} from "react";
+import AppBody from "./AppBody.js";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Workspaces </h1>
+        <AppBody/>
+      </div>
+ );
+     
+   
+  }
+}
+
+export default App;
